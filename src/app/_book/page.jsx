4498355b4831789a7a69edbff5a172030a7dff5c
dilruba-sarker <dilrubasarkerseo@gmail.com>
2025-08-05 23:37:12 +0600

    private route

---
+++ src/app/_book/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import BookComponent from './book'
+
+export default function BookPage() {
+  return (
+    <div>
+        
+    <BookComponent></BookComponent>
+
+
+    </div>
+  )
+}
